ba7db5ef49a1f44866d612a3d66a60f9a72ff621
Jarvis <610090238@qq.com>
2024-06-08 18:10:50 +0800

    feat: 二叉树前序遍历

---
+++ binary_tree/144.go
@@ -0,0 +1,31 @@
+package binary_tree
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	var res []int
+	var traversal func(node *TreeNode)
+	traversal = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		res = append(res, node.Val)
+		traversal(node.Left)
+		traversal(node.Right)
+	}
+	traversal(root)
+	return res
+}
