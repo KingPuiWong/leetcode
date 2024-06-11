2ae6d5e233037ae4b6934fc1fd8e69064369f0f3
Jarvis <610090238@qq.com>
2024-06-12 00:11:45 +0800

    feat: binary_tree:102

--- binary_tree/102.go
+++ binary_tree/102.go
@@ -1,1 +1,35 @@
 package binary_tree
+
+import (
+	"container/list"
+)
+
+func levelOrder(root *TreeNode) [][]int {
+	var res [][]int
+
+	if root == nil {
+		return res
+	}
+
+	queue := list.New()
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		length := queue.Len()
+		var arr []int
+		for i := 0; i < length; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+
+			arr = append(arr, node.Val)
+		}
+
+		res = append(res, arr)
+	}
+	return res
+}
